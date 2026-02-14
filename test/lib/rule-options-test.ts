aed5575ba6d586bd23dba3f06ffa21825efe26f8
Jason Weinzierl <weinzierljason@gmail.com>
2026-02-14 16:31:00 -0600

    feat: Use rule option defaults from `meta.defaultOptions` with fallback to schema default  (#885)
    Co-authored-by: Bryan Mishkin <bryan.mishkin@vercel.com>

--- test/lib/rule-options-test.ts
+++ test/lib/rule-options-test.ts
@@ -272,5 +272,312 @@ describe('rule options', function () {
         ]
       `);
     });
+
+    describe('with meta.defaultOptions', function () {
+      it('uses defaultOptions over schema defaults', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false, // This should be overridden
+                  },
+                  bar: {
+                    type: 'string',
+                    enum: ['always', 'never'],
+                    default: 'always', // This should be overridden
+                  },
+                  baz: {
+                    type: 'number',
+                    // No schema default
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            [{ foo: true, bar: 'never', baz: 42 }], // meta.defaultOptions takes priority
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": true,
+              "name": "foo",
+              "type": "Boolean",
+            },
+            {
+              "default": "never",
+              "enum": [
+                "always",
+                "never",
+              ],
+              "name": "bar",
+              "type": "String",
+            },
+            {
+              "default": 42,
+              "name": "baz",
+              "type": "Number",
+            },
+          ]
+        `);
+      });
+
+      it('falls back to schema defaults when defaultOptions does not contain the option', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false,
+                  },
+                  bar: {
+                    type: 'string',
+                    default: 'always',
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            [{ foo: true }], // Only overrides foo, bar should use schema default
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": true,
+              "name": "foo",
+              "type": "Boolean",
+            },
+            {
+              "default": "always",
+              "name": "bar",
+              "type": "String",
+            },
+          ]
+        `);
+      });
+
+      it('handles empty defaultOptions', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false,
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            [{}], // Empty defaultOptions object
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": false,
+              "name": "foo",
+              "type": "Boolean",
+            },
+          ]
+        `);
+      });
+
+      it('handles undefined defaultOptions', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false,
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            undefined, // No defaultOptions
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": false,
+              "name": "foo",
+              "type": "Boolean",
+            },
+          ]
+        `);
+      });
+
+      it('handles multiple schema items with defaultOptions array', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false,
+                  },
+                },
+                additionalProperties: false,
+              },
+              {
+                type: 'object',
+                properties: {
+                  bar: {
+                    type: 'string',
+                    default: 'original',
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            [{ foo: true }, { bar: 'overridden' }], // defaultOptions for each schema item
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": true,
+              "name": "foo",
+              "type": "Boolean",
+            },
+            {
+              "default": "overridden",
+              "name": "bar",
+              "type": "String",
+            },
+          ]
+        `);
+      });
+
+      it('handles nested object properties in defaultOptions', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  reporting: {
+                    type: 'object',
+                    properties: {
+                      verbose: {
+                        type: 'boolean',
+                        default: false,
+                      },
+                      format: {
+                        type: 'string',
+                        default: 'text',
+                      },
+                    },
+                    additionalProperties: false,
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            [{ reporting: { verbose: true, format: 'json' } }],
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": {
+                "format": "json",
+                "verbose": true,
+              },
+              "name": "reporting",
+              "type": "Object",
+            },
+            {
+              "default": true,
+              "name": "verbose",
+              "type": "Boolean",
+            },
+            {
+              "default": "json",
+              "name": "format",
+              "type": "String",
+            },
+          ]
+        `);
+      });
+
+      it('gracefully ignores non-array defaultOptions when schema is an array', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    default: false,
+                  },
+                },
+                additionalProperties: false,
+              },
+            ],
+            { foo: true }, // Malformed: should be an array to match the schema array
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": false,
+              "name": "foo",
+              "type": "Boolean",
+            },
+          ]
+        `);
+      });
+
+      it('preserves other properties when using defaultOptions', function () {
+        expect(
+          getAllNamedOptions(
+            [
+              {
+                type: 'object',
+                properties: {
+                  foo: {
+                    type: 'boolean',
+                    description: 'A boolean option',
+                    deprecated: true,
+                    enum: [true, false],
+                    default: false,
+                  },
+                },
+                required: ['foo'],
+                additionalProperties: false,
+              },
+            ],
+            [{ foo: true }], // defaultOptions overrides default
+          ),
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "default": true,
+              "deprecated": true,
+              "description": "A boolean option",
+              "enum": [
+                true,
+                false,
+              ],
+              "name": "foo",
+              "required": true,
+              "type": "Boolean",
+            },
+          ]
+        `);
+      });
+    });
   });
 });
